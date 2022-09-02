585f3df9603e4e34d9d999915427b4d3ae280984
jdots <jessndoty@gmail.com>
2022-09-02 00:08:53 -0700

    modules

---
+++ src/helpers.js
@@ -0,0 +1,14 @@
+function choice(items) {
+    const idx = Math.floor(Math.random()*(items.length))
+    return items[idx]
+}
+
+function remove(items, item) {
+    const idx = items.indexOf(item);
+    if (idx > -1) {
+        items.splice(idx, 1)
+    }
+    return items
+}
+
+export {choice, remove};
--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {choice, remove} from "./helpers";
+import foods from "./foods";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const randFruit = choice(foods);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+console.log(`I'd like one ${randFruit}, please.`);
+console.log(`Here you go: ${randFruit}`);
+console.log(`Delicious! May I have another ${randFruit}?`);
+
+const fruitsLeft = remove(foods, randFruit);
+console.log(`I'm sorry, we're all out. We have ${fruitsLeft.length} other fruits left.`)
+
